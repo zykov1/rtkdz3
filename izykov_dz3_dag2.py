1e16e8e9f99ed78b320a3537cd1727c6f2cd4562
zykov1 <76252513+zykov1@users.noreply.github.com>
2021-04-14 14:51:05 +0300

    Update izykov_dz3_dag2.py

--- izykov_dz3_dag2.py
+++ izykov_dz3_dag2.py
@@ -23,10 +23,8 @@ dag = DAG(
     schedule_interval = '0 0 1 1 *',
 )
 
-ods_billing = DataProcHiveOperator(
-    task_id = user_name + '_ods_billing',
-    dag = dag,
-    query = """
+ods_tables = {
+    'billing': """
         insert overwrite table izykov.ods_billing partition (year = {{ execution_date.year }}) select
             cast(user_id as int), -- Если поле меняет тип при etl - я специально явно указал cast
             /*
@@ -43,16 +41,7 @@ ods_billing = DataProcHiveOperator(
             cast(created_at as date)
         from izykov.stg_billing where year(created_at) = {{ execution_date.year }};
     """,
-    cluster_name='cluster-dataproc',
-    job_name = user_name + '_ods_billing_{{ execution_date.year }}_{{ params.job_suffix }}',
-    params = {"job_suffix": randint(0, 100000)},
-    region = 'europe-west3',
-)
-
-ods_issue = DataProcHiveOperator(
-    task_id = user_name + '_ods_issue',
-    dag = dag,
-    query = """
+    'issue': """
         insert overwrite table izykov.ods_issue partition (year = {{ execution_date.year }}) select
             cast(user_id as int),
             cast(start_time as timestamp),
@@ -62,16 +51,7 @@ ods_issue = DataProcHiveOperator(
             service
         from izykov.stg_issue where year(start_time) = {{ execution_date.year }};
     """,
-    cluster_name='cluster-dataproc',
-    job_name = user_name + '_ods_issue_{{ execution_date.year }}_{{ params.job_suffix }}',
-    params = {"job_suffix": randint(0, 100000)},
-    region = 'europe-west3',
-)
-
-ods_payment = DataProcHiveOperator(
-    task_id = user_name + '_ods_payment',
-    dag = dag,
-    query = """
+    'payment': """
         insert overwrite table izykov.ods_payment partition (year = {{ execution_date.year }}) select
             cast(user_id as int),
             pay_doc_type,
@@ -83,16 +63,7 @@ ods_payment = DataProcHiveOperator(
             cast(sum as int)
         from izykov.stg_payment where year(pay_date) = {{ execution_date.year }};
     """,
-    cluster_name='cluster-dataproc',
-    job_name = user_name + '_ods_payment_{{ execution_date.year }}_{{ params.job_suffix }}',
-    params = {"job_suffix": randint(0, 100000)},
-    region = 'europe-west3',
-)
-
-ods_traffic = DataProcHiveOperator(
-    task_id = user_name + '_ods_traffic',
-    dag = dag,
-    query = """
+    'traffic': """
         insert overwrite table izykov.ods_traffic partition (year = {{ execution_date.year }}) select
             cast(user_id as int),
             cast(cast(`timestamp` as bigint) as timestamp), -- Двойная конвертация позволяет сохранить имеющиеся миллисекунды!
@@ -112,26 +83,43 @@ ods_traffic = DataProcHiveOperator(
             cast(bytes_received as bigint)
         -- Ниже - проверил - только двойной cast дает нормальный результат в year - либо надо обрезать миллисекунды
         from izykov.stg_traffic where year(cast(cast(`timestamp` as bigint) as timestamp)) = {{ execution_date.year }};
-        /*
-            Ниже - обновление витрины dm_traffic, можно было вынести в отдельный DAG, например,
-            если до 11 января этими данными все равно никто не пользуется - то обновлять 10-го.
-            Пояснения по витрине:
-            Несмотря на свежую версию hive (может, конкретная сборка виновата?), никак не работал вариант
-            с create materialized view ... partitioned on / by.
-            Были ошибки типа:
-            FAILED: ParseException line 1:52 mismatched input 'partitioned' expecting AS near 'rewrite' in create materialized view statement
-            Решил строить витрину все-таки на базе materialized view, а не простой таблицы, т.к.:
-            1) MatView исключает произвольную модификацию данных в нем - правильно с т.з.безопасности.
-            2) Hive может использовать MatView для ускорения других запросов автоматически (правда, для транзакционных таблиц).
-            3) В ДЗ не сказано использовать простую таблицу. Хотя идемпотентность (если matview без партиций) может нарушаться, НО!
-            ALTER MATERIALIZED VIEW - в документации сказано, что старается работать инкрементно - только с теми
-            строками, которые изменились.
-            4) Надеюсь, "partitioned on" вернут, тогда решение через matview с партициями - будет 100% верным.
-        */
-        alter materialized view izykov.dm_traffic rebuild;
-    """,
-    cluster_name='cluster-dataproc',
-    job_name = user_name + '_ods_traffic_{{ execution_date.year }}_{{ params.job_suffix }}',
-    params = {"job_suffix": randint(0, 100000)},
-    region = 'europe-west3',
-)
+    """
+}
+
+for table, hiveql in ods_tables.items():
+    dpho = DataProcHiveOperator(
+        task_id = user_name + '_ods_' + table,
+        dag = dag,
+        query = hiveql,
+        cluster_name='cluster-dataproc',
+        job_name = user_name + '_ods_' + table + '_{{ execution_date.year }}_{{ params.job_suffix }}',
+        params = {"job_suffix": randint(0, 100000)},
+        region = 'europe-west3',
+    )
+    if table == 'traffic':
+        dphoc = DataProcHiveOperator(
+            task_id = user_name + '_dm_' + table,
+            dag = dag,
+            query = """
+                /*
+                    Пояснения по витрине:
+                    Несмотря на свежую версию hive (может, конкретная сборка виновата?), никак не работал вариант
+                    с create materialized view ... partitioned on / by.
+                    Были ошибки типа:
+                    FAILED: ParseException line 1:52 mismatched input 'partitioned' expecting AS near 'rewrite' in create materialized view statement
+                    Решил строить витрину все-таки на базе materialized view, а не простой таблицы, т.к.:
+                    1) MatView исключает произвольную модификацию данных в нем - правильно с т.з.безопасности.
+                    2) Hive может использовать MatView для ускорения других запросов автоматически (правда, для транзакционных таблиц).
+                    3) В ДЗ не сказано использовать простую таблицу. Хотя идемпотентность (если matview без партиций) может нарушаться, НО!
+                    ALTER MATERIALIZED VIEW - в документации сказано, что старается работать инкрементно - только с теми
+                    строками, которые изменились.
+                    4) Надеюсь, "partitioned on" вернут, тогда решение через matview с партициями - будет 100% верным.
+                */
+                alter materialized view izykov.dm_traffic rebuild;
+            """,
+            cluster_name='cluster-dataproc',
+            job_name = user_name + '_ods_' + table + '_{{ execution_date.year }}_{{ params.job_suffix }}',
+            params = {"job_suffix": randint(0, 100000)},
+            region = 'europe-west3',
+        )
+        dpho >> dphoc
