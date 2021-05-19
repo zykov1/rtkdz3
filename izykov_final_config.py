c22875e3c6e2c691f348a837cdd55f39d96c7e22
zykov1 <76252513+zykov1@users.noreply.github.com>
2021-05-19 23:29:39 +0300

    Update izykov_final_config.py

--- izykov_final_config.py
+++ izykov_final_config.py
@@ -14,6 +14,19 @@ stg_tables = {
         LOCATION ('pxf://rt-2021-03-25-16-47-29-sfunu-final-project/billing/year={{ execution_date.year }}/?PROFILE=gs:parquet')
         FORMAT 'CUSTOM' (FORMATTER='pxfwritable_import');
     """,
+    'issue': """
+        DROP EXTERNAL TABLE izykov.p_stg_issue_{{ execution_date.year }};
+        CREATE EXTERNAL TABLE izykov.p_stg_issue_{{ execution_date.year }} (
+            user_id TEXT,
+            start_time TIMESTAMP,
+            end_time TIMESTAMP,
+            title TEXT,
+            description TEXT,
+            service TEXT
+        )
+        LOCATION ('pxf://rt-2021-03-25-16-47-29-sfunu-final-project/issue/year={{ execution_date.year }}/?PROFILE=gs:parquet')
+        FORMAT 'CUSTOM' (FORMATTER='pxfwritable_import');
+    """,
     'payment': """
         DROP EXTERNAL TABLE izykov.p_stg_payment_{{ execution_date.year }};
         CREATE EXTERNAL TABLE izykov.p_stg_payment_{{ execution_date.year }} (
@@ -29,4 +42,17 @@ stg_tables = {
         LOCATION ('pxf://rt-2021-03-25-16-47-29-sfunu-final-project/payment/year={{ execution_date.year }}/?PROFILE=gs:parquet')
         FORMAT 'CUSTOM' (FORMATTER='pxfwritable_import');
     """,
+    'traffic': """
+        DROP EXTERNAL TABLE izykov.p_stg_traffic_{{ execution_date.year }};
+        CREATE EXTERNAL TABLE izykov.p_stg_traffic_{{ execution_date.year }} (
+            user_id INT,
+            timestamp BIGINT,
+            device_id TEXT,
+            device_ip_addr TEXT,
+            bytes_sent INT,
+            bytes_received INT
+        )
+        LOCATION ('pxf://rt-2021-03-25-16-47-29-sfunu-final-project/traffic/year={{ execution_date.year }}/?PROFILE=gs:parquet')
+        FORMAT 'CUSTOM' (FORMATTER='pxfwritable_import');
+    """
 }
