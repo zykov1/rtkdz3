dcf5dc5dd2b68b3239fc9311654b8d7adf2b17b7
zykov1 <76252513+zykov1@users.noreply.github.com>
2021-05-19 21:27:19 +0300

    Update izykov_final_test.py

--- izykov_final_test.py
+++ izykov_final_test.py
@@ -4,24 +4,16 @@ from airflow import DAG
 from airflow.operators.postgres_operator import PostgresOperator
 from airflow.operators.dummy_operator import DummyOperator
 
-### Общие параметры и запросы
+### Конфиг (запросы)
 USERNAME = 'izykov'
 import sys
 sys.path.append('/root/airflow/dags/' + USERNAME)
 import izykov_final_config as c
 
-### Общий алгоритм
-def main():
-  start()
-  load_ods()
-  load_dds()
-  load_dm()
-  finish()
-  # print("that's all, folks")
-
+### Общие параметры DAG
 default_args = {
   "owner": USERNAME,
-  "start_date": datetime(2013, 1, 1, 0, 0, 0),
+  "begin_date": datetime(2013, 1, 1, 0, 0, 0),
   'depends_on_past': False,
   'email_on_failure': False,
   'email_on_retry': False,
@@ -35,27 +27,36 @@ dag = DAG(
   schedule_interval = "0 0 1 1 *"
 )
 
+### Общий алгоритм
+def main():
+  begin()
+  load_ods()
+  load_dds()
+  load_dm()
+  end()
+  # print("that's all, folks")
+
 ### Шаги общего алгоритма
-def start():
-  c.stg_start = DummyOperator(task_id = "stg_start", dag = dag)
+def begin():
+  c.stg_begin = DummyOperator(task_id = "stg_begin", dag = dag)
   return
 
 def load_ods():
-  c.stg_finish_ods_start = DummyOperator(task_id = "stg_finish_ods_start", dag = dag)
-  c.stg_start >> c.stg_finish_ods_start
+  c.stg_end_ods_begin = DummyOperator(task_id = "stg_end_ods_begin", dag = dag)
+  c.stg_begin >> c.stg_end_ods_begin
   return
 
 def load_dds():
-  c.ods_finish_dds_start = DummyOperator(task_id = "ods_finish_dds_start", dag = dag)
-  c.stg_finish_ods_start >> c.ods_finish_dds_start
+  c.ods_end_dds_begin = DummyOperator(task_id = "ods_end_dds_begin", dag = dag)
+  c.stg_end_ods_begin >> c.ods_end_dds_begin
   return
 
 def load_dm():
-  c.dds_finish_dm_start = DummyOperator(task_id = "dds_finish_dm_start", dag = dag)
-  c.ods_finish_dds_start >> c.dds_finish_dm_start
+  c.dds_end_dm_begin = DummyOperator(task_id = "dds_end_dm_begin", dag = dag)
+  c.ods_end_dds_begin >> c.dds_end_dm_begin
   return
 
-def finish():
+def end():
   return
 
 ### Запуск общего алгоритма
