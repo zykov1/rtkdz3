d960b7b8b9c47ba98c966d29b292b9ea7c11aba2
zykov1 <76252513+zykov1@users.noreply.github.com>
2021-05-21 10:38:17 +0300

    Update izykov_final_test.py

--- izykov_final_test.py
+++ izykov_final_test.py
@@ -1,5 +1,5 @@
 from datetime import timedelta, datetime
-
+# change test
 from airflow import DAG
 from airflow.operators.postgres_operator import PostgresOperator
 from airflow.operators.dummy_operator import DummyOperator
