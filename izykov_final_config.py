16c38be6b4ac01a27fb49da6c0432d87400d14f7
zykov1 <76252513+zykov1@users.noreply.github.com>
2021-05-21 12:41:52 +0300

    Update izykov_final_config.py

--- izykov_final_config.py
+++ izykov_final_config.py
@@ -15,21 +15,70 @@ for y in range(2013, 2031):
 ### Таблицы + SQL для заливки STG-слоя с учетом идемпотентности
 stg_tables = {
     'billing': """
-       ALTER TABLE izykov.p_stg_billing OWNER TO izykov;
+    SELECT izykov.runit ($$
+        CREATE EXTERNAL TABLE izykov.p_stg_billing (
+            user_id INT,
+            billing_period TEXT,
+            service TEXT,
+            tariff TEXT,
+            sum TEXT,
+            created_at DATE
+        )
+        LOCATION ('pxf://rt-2021-03-25-16-47-29-sfunu-final-project/billing/*/?PROFILE=gs:parquet')
+        FORMAT 'CUSTOM' (FORMATTER='pxfwritable_import');
+        ALTER TABLE izykov.p_stg_billing OWNER TO izykov;
+    $$) WHERE (TO_REGCLASS('izykov.p_stg_billing')) IS NULL;
     """,
 
     'issue': """
-    ALTER TABLE izykov.p_stg_issue OWNER TO izykov;
+    SELECT izykov.runit ($$
+        CREATE EXTERNAL TABLE izykov.p_stg_issue (
+            user_id TEXT, -- проблема исходника, тип привожу к int потом
+            start_time TIMESTAMP,
+            end_time TIMESTAMP,
+            title TEXT,
+            description TEXT,
+            service TEXT
+        )
+        LOCATION ('pxf://rt-2021-03-25-16-47-29-sfunu-final-project/issue/*/?PROFILE=gs:parquet')
+        FORMAT 'CUSTOM' (FORMATTER='pxfwritable_import');
+        ALTER TABLE izykov.p_stg_issue OWNER TO izykov;
+    $$) WHERE (TO_REGCLASS('izykov.p_stg_issue')) IS NULL;
     """,
 
     'payment': """
-     ALTER TABLE izykov.p_stg_payment OWNER TO izykov;
-""",
+    SELECT izykov.runit ($$
+        CREATE EXTERNAL TABLE izykov.p_stg_payment (
+            user_id INT,
+            pay_doc_type TEXT,
+            pay_doc_num INT,
+            account TEXT,
+            phone NUMERIC(11,0),
+            billing_period TEXT,
+            pay_date DATE,
+            sum FLOAT
+        )
+        LOCATION ('pxf://rt-2021-03-25-16-47-29-sfunu-final-project/payment/*/?PROFILE=gs:parquet')
+        FORMAT 'CUSTOM' (FORMATTER='pxfwritable_import');
+        ALTER TABLE izykov.p_stg_payment OWNER TO izykov;
+    $$) WHERE (TO_REGCLASS('izykov.p_stg_payment')) IS NULL;
+    """,
 
     'traffic': """
-ALTER TABLE izykov.p_stg_traffic OWNER TO izykov;
-
-"""
+    SELECT izykov.runit ($$
+        CREATE EXTERNAL TABLE izykov.p_stg_traffic (
+            user_id INT,
+            timestamp BIGINT,
+            device_id TEXT,
+            device_ip_addr TEXT,
+            bytes_sent INT,
+            bytes_received INT
+        )
+        LOCATION ('pxf://rt-2021-03-25-16-47-29-sfunu-final-project/traffic/*/?PROFILE=gs:parquet')
+        FORMAT 'CUSTOM' (FORMATTER='pxfwritable_import');
+        ALTER TABLE izykov.p_stg_traffic OWNER TO izykov;
+    $$) WHERE (TO_REGCLASS('izykov.p_stg_traffic')) IS NULL;
+    """
 }
 
 
@@ -156,4 +205,7 @@ ods_tables = {
         ;
     """,
 
+    'traffic': """
+
+    """,
 }
