2dd5f8e7da247cfd99e089f08cf6a1788a7839b8
zykov1 <76252513+zykov1@users.noreply.github.com>
2021-05-22 23:40:12 +0300

    Update and rename izykov_final_test3.py to izykov_final_test4.py

--- izykov_final_test4.py
+++ izykov_final_test4.py
@@ -22,7 +22,7 @@ default_args = {
     'retry_delay': timedelta(minutes = 3)
 }
 dag = DAG(
-    USERNAME + '_final_etl_test3',
+    USERNAME + '_final_etl_test4',
     default_args = default_args,
     description = USERNAME + ' FINAL ETL TEST',
     schedule_interval = "0 0 1 1 *",
