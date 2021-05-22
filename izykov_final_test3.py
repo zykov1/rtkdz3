ec2cbcb4bd9b649f8be057978d7b8dd25af170bb
zykov1 <76252513+zykov1@users.noreply.github.com>
2021-05-22 17:41:11 +0300

    Update izykov_final_test3.py

--- izykov_final_test3.py
+++ izykov_final_test3.py
@@ -1,5 +1,5 @@
 from datetime import timedelta, datetime
-# лосось
+
 from airflow import DAG
 from airflow.operators.postgres_operator import PostgresOperator
 from airflow.operators.dummy_operator import DummyOperator
@@ -26,20 +26,32 @@ dag = DAG(
     default_args = default_args,
     description = USERNAME + ' FINAL ETL TEST',
     schedule_interval = "0 0 1 1 *",
+    concurrency=1,
     max_active_runs = 1
 )
+c.stg_end_ods_begin = DummyOperator(task_id = "stg_end_ods_begin", dag = dag)
+c.ods_end_mviews_begin = DummyOperator(task_id = "ods_end_mviews_begin", dag = dag)
+c.mviews_end_dds_hubs_begin = DummyOperator(task_id = "mviews_end_dds_hubs_begin", dag = dag)
+# c.dds_hubs_end_dds_links_begin = DummyOperator(task_id = "dds_hubs_end_dds_links_begin", dag = dag)
+# c.dds_links_end_dds_sats_begin = DummyOperator(task_id = "dds_links_end_dds_sats_begin", dag = dag)
+# c.dds_sats_end_dm_begin = DummyOperator(task_id = "dds_sats_end_dm_begin", dag = dag)
+### собственно, лишние прямые связи - потом надо бы убрать
+# c.stg_begin >> c.stg_end_ods_begin >> c.ods_end_dds_begin >> c.dds_end_dm_begin
 
 ### Главный алгоритм
 def main():
-    begin()
+    load_stg()
     load_ods()
-    load_dds()
-    load_dm()
-    end()
+    load_mviews()
+    # load_dds_hubs()
+    # load_dds_links()
+    # load_dds_sats()
+    # load_dm()
+    # end()
 ### Конец    
 
 ### Детали главного алгоритма
-def begin():
+def load_stg():
     # c.stg_begin = DummyOperator(task_id = "stg_begin", dag = dag)
     c.stg_begin = PostgresOperator(
         task_id = "stg_begin",
@@ -53,10 +65,6 @@ def begin():
             ALTER FUNCTION izykov.runit(TEXT) OWNER TO izykov;
         """
     )
-    c.stg_end_ods_begin = DummyOperator(task_id = "stg_end_ods_begin", dag = dag)
-    c.ods_end_dds_begin = DummyOperator(task_id = "ods_end_dds_begin", dag = dag)
-    c.dds_end_dm_begin = DummyOperator(task_id = "dds_end_dm_begin", dag = dag)
-    c.stg_begin >> c.stg_end_ods_begin >> c.ods_end_dds_begin >> c.dds_end_dm_begin
     for table, sql in c.stg_tables.items():
         po = PostgresOperator(
             dag = dag,
@@ -73,17 +81,28 @@ def load_ods():
             task_id = 'ods_' + table + '_recreate',
             sql = sql
         )
-        c.stg_end_ods_begin >> po >> c.ods_end_dds_begin
+        c.stg_end_ods_begin >> po >> c.ods_end_mviews_begin
     return
 
-def load_dds():
+def load_mviews():
+    for table, sql in c.mviews.items():
+        po = PostgresOperator(
+            dag = dag,
+            task_id = 'mview_' + table + '_calc',
+            sql = sql
+        )
+        c.ods_end_mviews_begin >> po >> c.mviews_end_dds_hubs_begin
     return
 
-def load_dm():
-    return
 
-def end():
-    return
+# def load_dds():
+#     return
+
+# def load_dm():
+#     return
+
+# def end():
+#     return
 
 ### Запуск главного алгоритма
 main()
